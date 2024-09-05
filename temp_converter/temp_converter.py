1afa85e28fb912f807e0a48e2b27632194e98fed
Shadizon <andrewg24642@gmail.com>
2024-09-05 17:43:47 -0400

    Update temp_converter.py
    Made changes

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-
+"""
+    temp_converter.py
+    Andrew Gomez
+    09/05/2024
+    CSC138-EN
+"""
 # process
-def c2f(c):
-    return c * 9 / 5 + 32
-
+def f2c(c):
+    return (9.0 / 5.0 * c) + 32
+    #Converts the input from Celsius to Fahrenheit
 def main(cel):
-    return c2f(cel)
+    return f2c(cel)
 
 if __name__ == "__main__":
     cel = 100         # input
